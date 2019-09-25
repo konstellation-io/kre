5147d4c06c1e3f33a512c9fe926af1bd2a9301eb
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2019-09-25 14:10:53 +0200

    create react app

---
+++ client/src/constants/routes.ts
@@ -0,0 +1,5 @@
+export const HOME = '/';
+// export const CREATE_LOCAL_CLUSTER = '/create-local-cluster';
+// export const INSTALL_LOCAL_CLUSTER = '/install-local-cluster';
+// export const SHOW_CLUSTER_PROJECTS = '/show-cluster-projects';
+// export const CREATE_PROJECT = '/create-project';
---
+++ client/src/pages/Login/Login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Login.module.scss';
+
+function Login() {
+  return (
+    <div>
+      Login
+    </div>
+  );
+}
+
+export default Login;
