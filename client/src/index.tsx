e008528d4bab38fa9866dc64e439c6169ff9d1fe
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2019-10-25 09:20:58 +0200

    feat(client): apollo client added

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,11 +3,28 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import App from './App';
+import { ApolloProvider } from '@apollo/react-hooks';
 import './styles/app.global.scss';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: 'http://localhost:4000/'
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
 ReactDOM.render(
-  <Provider store={configureStore()}>
-    <App />
-  </Provider>,
+  <ApolloProvider client={client}>
+    <Provider store={configureStore()}>
+      <App />
+    </Provider>
+  </ApolloProvider>,
   document.getElementById('root')
 );
--- client/src/pages/Dashboard/Dashboard.tsx
+++ client/src/pages/Dashboard/Dashboard.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import HexagonPanel from '../../components/Layout/HexagonPanel/HexagonPanel';
 import Hexagon from '../../components/Shape/Hexagon/Hexagon';
-import * as MOCK_DATA from '../../constants/mock';
+import Spinner from '../../components/Spinner/Spinner';
+import { formatRuntime } from './dataModel';
 import styles from './Dashboard.module.scss';
 
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+const GET_RUNTIMES = gql`
+  query GetRuntimes {
+    runtimes {
+      id
+      name
+      status
+      creationDate
+    }
+  }
+`;
+
 function Dashboard() {
-  const runtimes = MOCK_DATA.RUNTIMES.map((runtime, idx) => (
-    <Hexagon key={`runtimeHexagon-${idx}`} {...runtime} />
+  const { data, loading, error } = useQuery(GET_RUNTIMES);
+  if (loading) return <Spinner />;
+  if (error) return <p>ERROR</p>;
+
+  const runtimes = data.runtimes.map((runtime:any, idx:number) => (
+    <Hexagon
+      key={`runtimeHexagon-${idx}`}
+      {...formatRuntime(runtime)}
+    />
   ));
 
   return (
