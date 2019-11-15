e137bbf826e5b48dbeb80ce05eceebbc600be51c
Gustavo Marin <gustavo.marin@intelygenz.com>
2019-11-15 14:40:20 +0100

    fix(operator): fix operator sdk install script

--- operator/scripts/install_operator_sdk.sh
+++ operator/scripts/install_operator_sdk.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-set -x
+set -ex
 
-apk add curl --update
+apk update && apk upgrade && apk add curl
 
 curl -LO https://github.com/operator-framework/operator-sdk/releases/download/${SDK_RELEASE_VERSION}/operator-sdk-${SDK_RELEASE_VERSION}-x86_64-linux-gnu
 chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu 
