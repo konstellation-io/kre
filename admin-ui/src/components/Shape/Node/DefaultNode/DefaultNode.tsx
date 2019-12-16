a9d248994c1d570a53680f22b45bf0abc1748334
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2019-12-16 11:15:17 +0100

    default version status node svg added

---
+++ admin-ui/src/components/Shape/Node/DefaultNode/DefaultNode.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ReactComponent as DefaultNodeSVG } from './DefaultNode.svg';
+
+function DefaultNode() {
+  return (
+    <div>
+      <DefaultNodeSVG />
+    </div>
+  );
+}
+
+export default DefaultNode;
--- admin-ui/src/pages/Runtime/pages/RuntimeStatusPreview/RuntimeStatusPreview.tsx
+++ admin-ui/src/pages/Runtime/pages/RuntimeStatusPreview/RuntimeStatusPreview.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import DefaultNode from '../../../../components/Shape/Node/DefaultNode/DefaultNode';
 
 function RuntimeStatusPreview() {
-  return <>STATUS PREVIEW</>;
+  return (
+    <>
+      STATUS PREVIEW
+      <DefaultNode />
+    </>
+  );
 }
 
 export default RuntimeStatusPreview;
