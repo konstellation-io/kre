ee01f1a67cc4a1531758df30682436ef58157cd8
Sergio Fernandez <sergio.fernandez@intelygenz.com>
2019-12-18 18:22:55 +0100

    feat(entrypoint): Add Nats integration on entrypoint

--- runtime-entrypoint/main.go
+++ runtime-entrypoint/main.go
@@ -2,31 +2,64 @@ package main
 
 import (
 	"context"
+	"fmt"
 	entrypoint "gitlab.com/konstellation/konstellation-ce/kre/runtime-entrypoint/proto"
 	"google.golang.org/grpc/reflection"
 	"net"
+	"os"
+	"time"
 
 	"github.com/prometheus/common/log"
 	"google.golang.org/grpc"
 
-	nats "github.com/nats-io/nats.go"
+	"github.com/nats-io/nats.go"
 )
 
 type GRPCServerWrapper struct {
 	*grpc.Server
-	*nc.
+	*NatsInfo
 }
 
-func (G GRPCServerWrapper) Ping(context.Context, *entrypoint.PingRequest) (*entrypoint.PingResponse, error) {
+type NatsInfo struct {
+	ConnUrl string
+	Channel string
+	nc      *nats.Conn
+}
+
+func (g GRPCServerWrapper) Ping(ctx context.Context, request *entrypoint.PingRequest) (*entrypoint.PingResponse, error) {
 	log.Info("Ping success")
-	nc.Publish("foo", []byte("Hello World"))
+
+	err := g.nc.Publish(g.Channel, []byte(fmt.Sprintf("Ping Received %s", time.Now())))
+	if err != nil {
+		return nil, err
+	}
+
 	return &entrypoint.PingResponse{
 		Success: true,
 	}, nil
 }
 
-func (G GRPCServerWrapper) initNats (){
-	nc, _ := nats.Connect("localhost:4223")
+func initNats() *NatsInfo {
+	connUrl, channel := os.Getenv("NATS_URL"), os.Getenv("NATS_CHANNEL")
+	if connUrl == "" {
+		connUrl = "kre-nats:4222"
+	}
+
+	if channel == "" {
+		channel = "entrypoint"
+	}
+
+	nc, err := nats.Connect(connUrl)
+
+	if err != nil {
+		panic("Error initializing NATS connection")
+	}
+
+	return &NatsInfo{
+		connUrl,
+		channel,
+		nc,
+	}
 }
 
 func main() {
@@ -39,7 +72,7 @@ func main() {
 	var opts []grpc.ServerOption
 	grpcServer := grpc.NewServer(opts...)
 	reflection.Register(grpcServer)
-	grpcServerWrapper := &GRPCServerWrapper{grpcServer}
+	grpcServerWrapper := &GRPCServerWrapper{grpcServer, initNats()}
 	entrypoint.RegisterEchoServiceServer(grpcServer, grpcServerWrapper)
 	log.Infof("Grpc server listen in %s ", urlConnection)
 
