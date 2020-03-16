e9faa1ae2a89b81d5f6b089435c0c0ed1881446a
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2020-03-16 11:42:05 +0100

    user activity message generator tests added

--- admin-ui/src/pages/UsersActivity/components/UserActivityList/UserActivityList.spec.tsx
+++ admin-ui/src/pages/UsersActivity/components/UserActivityList/UserActivityList.spec.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import moment from 'moment-timezone';
 
 import UserActivityList from './UserActivityList';
+import getMessage, { VarTypes } from './messageGenerator';
 import { formatDate } from '../../../../utils/format';
 import { cleanup } from '@testing-library/react';
 
 import { usersActivityMock } from '../../../../mocks/settings';
 import { shallow } from 'enzyme';
 import { testid } from '../../../../utils/testUtilsEnzyme';
+import { UserActivityType } from '../../../../graphql/types/globalTypes';
 
 const mockData = usersActivityMock.result.data.userActivityList;
 
@@ -37,5 +39,170 @@ describe('UserActivityList', () => {
     expect(user0.text()).toBe('user1@domain.com');
     expect(message0.text()).toBe('Has logged in');
     expect(date1.text()).toBe(dateFormatted);
+  });
+});
+
+describe('messageGenerator', () => {
+  let wrapper;
+  const userActivity = {
+    id: 'someId',
+    user: {
+      email: 'user@mail.com'
+    },
+    date: '2020-01-01',
+    vars: []
+  };
+  const runtime = [
+    {
+      key: VarTypes.RUNTIME_ID,
+      value: 'runtimeId'
+    },
+    {
+      key: VarTypes.RUNTIME_NAME,
+      value: 'runtimeName'
+    }
+  ];
+  const version = [
+    {
+      key: VarTypes.VERSION_ID,
+      value: 'versionId'
+    },
+    {
+      key: VarTypes.VERSION_NAME,
+      value: 'versionName'
+    }
+  ];
+  const oldVersion = [
+    {
+      key: VarTypes.OLD_PUBLISHED_VERSION_ID,
+      value: 'oldVersionId'
+    },
+    {
+      key: VarTypes.OLD_PUBLISHED_VERSION_NAME,
+      value: 'oldVersionName'
+    }
+  ];
+  const setting = [
+    {
+      key: VarTypes.SETTING_NAME,
+      value: 'setting'
+    },
+    {
+      key: VarTypes.OLD_VALUE,
+      value: 'oldValue'
+    },
+    {
+      key: VarTypes.NEW_VALUE,
+      value: 'newValue'
+    }
+  ];
+  const config = [
+    {
+      key: VarTypes.CONFIG_KEYS,
+      value: 'confA, confB, confC'
+    }
+  ];
+
+  function setWrapper(props) {
+    const [component, comment] = getMessage({ ...userActivity, ...props });
+    wrapper = shallow(<div>{component}</div>);
+  }
+
+  it('show right LOGIN message', () => {
+    setWrapper({ type: UserActivityType.LOGIN });
+
+    expect(wrapper.text()).toBe('Has logged in');
+  });
+
+  it('show right LOGOUT message', () => {
+    setWrapper({ type: UserActivityType.LOGOUT });
+
+    expect(wrapper.text()).toBe('Has logged out');
+  });
+
+  it('show right CREATE_RUNTIME message', () => {
+    setWrapper({
+      type: UserActivityType.CREATE_RUNTIME,
+      vars: [...runtime]
+    });
+
+    expect(wrapper.text()).toBe('Has created a new Runtime: runtimeName');
+  });
+
+  it('show right CREATE_VERSION message', () => {
+    setWrapper({
+      type: UserActivityType.CREATE_VERSION,
+      vars: [...runtime, ...version]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has created a new Version: runtimeName - versionName'
+    );
+  });
+
+  it('show right PUBLISH_VERSION message', () => {
+    setWrapper({
+      type: UserActivityType.PUBLISH_VERSION,
+      vars: [...runtime, ...version, ...oldVersion]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has published version: runtimeName - versionName. Previous published version: oldVersionName'
+    );
+  });
+
+  it('show right UNPUBLISH_VERSION message', () => {
+    setWrapper({
+      type: UserActivityType.UNPUBLISH_VERSION,
+      vars: [...runtime, ...version]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has unpublished version runtimeName - versionName'
+    );
+  });
+
+  it('show right STOP_VERSION message', () => {
+    setWrapper({
+      type: UserActivityType.STOP_VERSION,
+      vars: [...runtime, ...version]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has stopped version runtimeName - versionName'
+    );
+  });
+
+  it('show right START_VERSION message', () => {
+    setWrapper({
+      type: UserActivityType.START_VERSION,
+      vars: [...runtime, ...version]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has started version runtimeName - versionName'
+    );
+  });
+
+  it('show right UPDATE_SETTING message', () => {
+    setWrapper({
+      type: UserActivityType.UPDATE_SETTING,
+      vars: [...setting]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has updated setting setting from oldValue to newValue'
+    );
+  });
+
+  it('show right UPDATE_VERSION_CONFIGURATION message', () => {
+    setWrapper({
+      type: UserActivityType.UPDATE_VERSION_CONFIGURATION,
+      vars: [...runtime, ...version, ...config]
+    });
+
+    expect(wrapper.text()).toBe(
+      'Has changed settings: confA, confB, confC from version runtimeName - versionName'
+    );
   });
 });
