f2da44665b2b83c982b79ce03d29ae2fef33ba17
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2020-03-25 18:25:36 +0100

    tests fixed

--- admin-ui/src/pages/Version/pages/Metrics/components/DashboardTitle/DashboardTitle.spec.tsx
+++ admin-ui/src/pages/Version/pages/Metrics/components/DashboardTitle/DashboardTitle.spec.tsx
@@ -2,12 +2,21 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import DashboardTitle from './DashboardTitle';
 
+jest.mock('react-router', () => ({
+  useParams: jest.fn(() => ({ runtimeId: '', versionId: '' }))
+}));
+
 describe('DashboardTitle', () => {
   let wrapper;
 
   beforeEach(() => {
     wrapper = shallow(
-      <DashboardTitle runtimeName="runtimeName" versionName="versionName" />
+      <DashboardTitle
+        runtimeName="runtimeName"
+        versionName="versionName"
+        formSetValue={jest.fn()}
+        formWatch={jest.fn()}
+      />
     );
   });
 
