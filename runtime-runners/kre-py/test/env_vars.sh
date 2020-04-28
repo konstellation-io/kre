adb9f31d75ab98f1e4cc3e7777492e35559cc535
asanchez <angel.sanchez@intelygenz.com>
2020-04-28 12:09:28 +0200

    fix env_vars.sh must not be versioned

--- runtime-runners/kre-py/test/env_vars.sh
+++
@@ -1,8 +0,0 @@
-export KRT_VERSION=testVersion1
-export KRT_NODE_NAME=nodeA
-export KRT_NATS_SERVER=localhost:4222
-export KRT_NATS_INPUT=test-subject-input
-export KRT_NATS_OUTPUT=
-export KRT_NATS_MONGO_WRITER=test-subject-mongo-writer
-export KRT_BASE_PATH=/home/asanchez/projects/kre/runtime-runners/kre-py/test/myvol
-export KRT_HANDLER_PATH=src/node/node_handler.py
