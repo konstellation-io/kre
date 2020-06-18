c9583dc065a77b9bee1d8ce7d94e6a86f9587ef0
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2020-06-18 16:08:24 +0200

    feat(admin-ui): remove allowed emails from admin ui

--- admin-ui/src/pages/Settings/SecuritySettings.tsx
+++ admin-ui/src/pages/Settings/SecuritySettings.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import SettingsHeader from './components/SettingsHeader/SettingsHeader';
 import AllowedDomains from './components/AllowedDomains/AllowedDomains';
-import AllowedEmails from './components/AllowedEmails/AllowedEmails';
 
 import cx from 'classnames';
 import styles from './Settings.module.scss';
@@ -12,21 +10,7 @@ function SecuritySettings() {
     <>
       <div className={cx(styles.form, styles.securitySettings)}>
         <SettingsHeader>Security settings</SettingsHeader>
-        <Tabs>
-          <div className={styles.tabs}>
-            <TabList>
-              <Tab>Domains</Tab>
-              <Tab>Emails</Tab>
-            </TabList>
-          </div>
-
-          <TabPanel>
-            <AllowedDomains />
-          </TabPanel>
-          <TabPanel>
-            <AllowedEmails />
-          </TabPanel>
-        </Tabs>
+        <AllowedDomains />
       </div>
     </>
   );
