0a4f03522a368c72a970b6b26a261a1897613a27
asanchez <angel.sanchez@intelygenz.com>
2020-07-10 14:18:35 +0200

    use library to copy folders

--- admin/admin-api/adapter/version/doc_generator.go
+++ admin/admin-api/adapter/version/doc_generator.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 	"github.com/konstellation-io/kre/admin/admin-api/adapter/config"
 	"github.com/konstellation-io/kre/admin/admin-api/domain/usecase/logging"
-	"io"
+	"github.com/otiai10/copy"
 	"io/ioutil"
 	"net/url"
 	"os"
@@ -43,7 +43,7 @@ func (g *HTTPStaticDocGenerator) Generate(versionID, docFolder string) error {
 	destDocFolder := path.Join(g.cfg.Admin.StoragePath, versionDocSubfolder)
 
 	g.logger.Infof("[HTTPStaticDocGenerator] Generating doc folder at: %s", destDocFolder)
-	err = copyDir(docFolder, destDocFolder)
+	err = copy.Copy(docFolder, destDocFolder)
 	if err != nil {
 		return fmt.Errorf("error copying doc folder: %w", err)
 	}
@@ -98,106 +98,4 @@ func (g *HTTPStaticDocGenerator) replaceRelativeImgs(file []byte, versionSubfold
 	replacement := fmt.Sprintf("![$1](%s/$2)", u.String())
 
 	return imgRegExp.ReplaceAll(file, []byte(replacement)), nil
-}
-
-// CopyFile copies the contents of the file named src to the file named
-// by dst. The file will be created if it does not already exist. If the
-// destination file exists, all it's contents will be replaced by the contents
-// of the source file. The file mode will be copied from the source and
-// the copied data is synced/flushed to stable storage.
-func copyFile(src, dst string) (err error) {
-	in, err := os.Open(src)
-	if err != nil {
-		return
-	}
-	defer in.Close()
-
-	out, err := os.Create(dst)
-	if err != nil {
-		return
-	}
-	defer func() {
-		if e := out.Close(); e != nil {
-			err = e
-		}
-	}()
-
-	_, err = io.Copy(out, in)
-	if err != nil {
-		return
-	}
-
-	err = out.Sync()
-	if err != nil {
-		return
-	}
-
-	si, err := os.Stat(src)
-	if err != nil {
-		return
-	}
-	err = os.Chmod(dst, si.Mode())
-	if err != nil {
-		return
-	}
-
-	return
-}
-
-// CopyDir recursively copies a directory tree, attempting to preserve permissions.
-// Source directory must exist, destination directory must *not* exist.
-// Symlinks are ignored and skipped.
-func copyDir(src string, dst string) (err error) {
-	src = filepath.Clean(src)
-	dst = filepath.Clean(dst)
-
-	si, err := os.Stat(src)
-	if err != nil {
-		return err
-	}
-	if !si.IsDir() {
-		return fmt.Errorf("source is not a directory")
-	}
-
-	_, err = os.Stat(dst)
-	if err != nil && !os.IsNotExist(err) {
-		return
-	}
-	if err == nil {
-		return fmt.Errorf("destination already exists")
-	}
-
-	err = os.MkdirAll(dst, si.Mode())
-	if err != nil {
-		return
-	}
-
-	entries, err := ioutil.ReadDir(src)
-	if err != nil {
-		return
-	}
-
-	for _, entry := range entries {
-		srcPath := filepath.Join(src, entry.Name())
-		dstPath := filepath.Join(dst, entry.Name())
-
-		if entry.IsDir() {
-			err = copyDir(srcPath, dstPath)
-			if err != nil {
-				return
-			}
-		} else {
-			// Skip symlinks.
-			if entry.Mode()&os.ModeSymlink != 0 {
-				continue
-			}
-
-			err = copyFile(srcPath, dstPath)
-			if err != nil {
-				return
-			}
-		}
-	}
-
-	return
 }
