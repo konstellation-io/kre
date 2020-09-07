ca5895a86d121c19d2308872db1102b3f71d1ff7
Mariano Gonzalez Salazar <mariano.gonzalez@intelygenz.com>
2020-09-07 15:21:51 +0200

    fix(admin-ui): entrypoint url tooltip now grows with entrypoint length

--- admin/admin-ui/src/Pages/Version/pages/Status/components/Tooltip/EntrypointTooltipContent.tsx
+++ admin/admin-ui/src/Pages/Version/pages/Status/components/Tooltip/EntrypointTooltipContent.tsx
@@ -1,30 +1,19 @@
-import React, { useRef } from 'react';
-
 import { Button } from 'kwc';
 import IconCopy from '@material-ui/icons/FileCopy';
+import React from 'react';
+import { copyToClipboard } from 'Utils/clipboard';
 import styles from './EntrypointTooltipContent.module.scss';
 
 type InputProps = {
   entrypointAddress: string;
 };
 export function EntrypointTooltipContent({ entrypointAddress }: InputProps) {
-  const inputRef = useRef<HTMLInputElement>(null);
-
-  function onCopyToClipboard() {
-    if (inputRef.current !== null) {
-      inputRef.current.select();
-      inputRef.current.setSelectionRange(0, 99999);
-
-      document.execCommand('copy');
-    }
-  }
-
   return (
     <div className={styles.wrapper}>
-      <input type="text" value={entrypointAddress} ref={inputRef} readOnly />
+      <span className={styles.address}>{entrypointAddress}</span>
       <Button
         label=""
-        onClick={onCopyToClipboard}
+        onClick={() => copyToClipboard(entrypointAddress)}
         Icon={IconCopy}
         className={styles.copyButton}
       />
