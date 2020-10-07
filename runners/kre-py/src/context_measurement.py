048cd6a09eb7a01757b1bf0aa7bff7e1a404955b
asanchez <angel.sanchez@intelygenz.com>
2020-10-07 17:27:19 +0200

    feat: set time and precision on save measurement

--- runners/kre-py/src/context_measurement.py
+++ runners/kre-py/src/context_measurement.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from influxdb_client import InfluxDBClient, Point, WritePrecision
-from influxdb_client.client.write_api import SYNCHRONOUS
+from influxdb_client.client.write_api import WriteOptions, WriteType
 
 
 # We are using influxdb-client-python that is compatible with 1.8+ versions:
@@ -12,14 +12,21 @@ INFLUX_TOKEN = ""  # we don't need authentication
 
 
 class ContextMeasurement:
+    PRECISION_S = WritePrecision.S
+    PRECISION_MS = WritePrecision.MS
+    PRECISION_US = WritePrecision.US
+    PRECISION_NS = WritePrecision.NS
+
     def __init__(self, config, logger):
         self.__config__ = config
         self.__logger__ = logger
 
         client = InfluxDBClient(url=self.__config__.influx_uri, token=INFLUX_TOKEN)
-        self.__write_api__ = client.write_api(write_options=SYNCHRONOUS)
 
-    def save(self, measurement: str, fields: dict, tags: dict):
+        opts = WriteOptions(write_type=WriteType.batching, batch_size=1_000, flush_interval=1_000)
+        self.__write_api__ = client.write_api(write_options=opts)
+
+    def save(self, measurement: str, fields: dict, tags: dict, time: datetime=None, precision=PRECISION_NS):
         point = Point(measurement)
 
         for key in fields:
@@ -30,6 +37,9 @@ class ContextMeasurement:
 
         point.tag("version", self.__config__.krt_version)
 
-        point.time(datetime.utcnow(), WritePrecision.NS)
+        if time is None:
+            time = datetime.utcnow()
+
+        point.time(time, precision)
 
         self.__write_api__.write(INFLUX_BUCKET, INFLUX_ORG, point)
