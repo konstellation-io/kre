beea500bae448ffe2a0c6c16f4cc83705c119f7d
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-01-13 23:21:43 +0000

    fix add version tests

--- engine/admin-ui/cypress/integration/addVersion.spec.ts
+++ engine/admin-ui/cypress/integration/addVersion.spec.ts
@@ -13,50 +13,14 @@ describe('Add Version Behavior', () => {
     cy.url().should('include', 'new_version');
   });
 
-  it('should fail if krt is not attached', () => {
+  it('should fail if file is not attached', () => {
     cy.getByTestId('createVersion').children().first().click();
     cy.getByTestId('uploadVersion').should('contain', 'This field is mandatory');
   });
 
-  xit('should work if krt is attached', () => {
-    cy.kstInterceptor('CreateVersion', {
-      data: {
-        createVersion: {
-          name:"greeter-v5",
-          description:"Version for testing.",
-          id: "61deb868ccbb9f8d0c105861",
-          status:"CREATING",
-          creationDate:"2022-01-12T11:15:52Z",
-          creationAuthor: {
-            id:"kre_admin_user",
-            email:"dev@local.local",
-            __typename:"User"
-          },
-          __typename:"Version"
-        }
-      }
-    });
-    //
-    // cy.kstInterceptor('GetVersionConfStatus', {
-    //   data:{
-    //     createVersion:{
-    //       id:"61deb868ccbb9f8d0c105861",
-    //       name:"greeter-v5",
-    //       description:"Version for testing.",
-    //       status:"CREATING",
-    //       creationDate:"2022-01-12T11:15:52Z",
-    //       creationAuthor:{
-    //         id:"kre_admin_user",
-    //         email:"dev@local.local",
-    //         __typename:"User"
-    //       },
-    //       __typename:"Version"
-    //     }
-    //   }
-    // });
-    cy.get('input').attachFile('greeter-v1.krt');
+  it('should fail if the file attached is not a .krt', () => {
+    cy.get('input').attachFile('metrics.json');
     cy.getByTestId('createVersion').children().first().click();
-    // cy.wait(50000)
-    cy.url().should('include', '/versions');
+    cy.getByTestId('uploadVersion').should('contain', 'Must be a .krt file');
   });
 });
