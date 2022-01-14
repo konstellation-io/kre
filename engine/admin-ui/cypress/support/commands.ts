816b1ef0bf7d20be7f17e47186c2948b04f83b5b
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-01-14 09:01:02 +0000

    delete useless comments on cypress files

--- engine/admin-ui/cypress/support/commands.ts
+++ engine/admin-ui/cypress/support/commands.ts
@@ -1,13 +1,3 @@
-import Chainable = Cypress.Chainable;
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
 Cypress.Commands.add('kstInterceptor', (operation, responseObject, options): Chainable => (
   cy.intercept('/graphql', (req) => {
     const { operationName } = req.body;
@@ -19,24 +9,10 @@ Cypress.Commands.add('kstInterceptor', (operation, responseObject, options): Cha
     req.reply(responseObject);
   })
 ));
+import Chainable = Cypress.Chainable;
 
 Cypress.Commands.add('getByTestId', (dataTestId) => cy.get(`[data-testid="${dataTestId}"]`));
 
 Cypress.Commands.add('findByTestId', { prevSubject: true }, (subject: Chainable<any>, dataTestId) =>
   subject.find(`[data-testid="${dataTestId}"]`),
 );
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
