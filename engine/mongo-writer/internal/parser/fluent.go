681fa7ebdecbbd87e9b46a681274404b02d98bb6
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-06-01 16:46:42 +0100

    fix log filter on mongo writer

--- engine/mongo-writer/internal/parser/fluent.go
+++ engine/mongo-writer/internal/parser/fluent.go
@@ -14,6 +14,7 @@ type LogMsg struct {
 	WorkflowName string `bson:"workflowName"`
 	NodeID       string `bson:"nodeId"`
 	NodeName     string `bson:"nodeName"`
+	RuntimeID    string `bson:"runtimeId"`
 	VersionID    string `bson:"versionId"`
 	VersionName  string `bson:"versionName"`
 }
@@ -73,6 +74,7 @@ func (f *fluentbitMsgParser) Parse(data []byte) ([]LogMsg, error) {
 			WorkflowName: msgData["workflowName"].(string),
 			NodeID:       msgData["nodeId"].(string),
 			NodeName:     msgData["nodeName"].(string),
+			RuntimeID:    msgData["runtimeId"].(string),
 			VersionID:    msgData["versionId"].(string),
 			VersionName:  msgData["versionName"].(string),
 		}
