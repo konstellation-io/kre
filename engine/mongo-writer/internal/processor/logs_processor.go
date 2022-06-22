7a74d4dadb4f7f85d519a59d93fd9c324f985db9
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-06-22 11:00:23 +0100

    fix golangci-lint on mongo-writer

--- engine/mongo-writer/internal/processor/logs_processor.go
+++ engine/mongo-writer/internal/processor/logs_processor.go
@@ -2,6 +2,7 @@ package processor
 
 import (
 	"context"
+
 	"github.com/konstellation-io/kre/engine/mongo-writer/internal/config"
 	"github.com/konstellation-io/kre/engine/mongo-writer/internal/logging"
 	"github.com/konstellation-io/kre/engine/mongo-writer/internal/mongodb"
