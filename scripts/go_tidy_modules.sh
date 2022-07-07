cdd887101cead0d3c44e59c71d86ebcc51e9735e
Alex Rodriguez <alex.rodriguez@intelygenz.com>
2022-07-07 15:52:22 +0200

    New script

---
+++ scripts/go_tidy_modules.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# execute from root of project
+
+cd engine/admin-api
+go mod tidy
+
+cd ../k8s-manager
+go mod tidy
+
+cd ../mongo-writer
+go mod tidy
+
+cd ../../libs/krt-utils/
+go mod tidy
+
+cd ../simplelogger/
+go mod tidy
+
+cd ../../tests/acceptance/
+go mod tidy
+
+cd ../acceptance/test_krt/runtime-test-v1/src/go-runner/
+go mod tidy
+
+cd ../..
+go work sync
