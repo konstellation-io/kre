8a3a57b9e24fcb2449b22625c08194e3f5e23b13
Juan Jesús Padrón Hernández <juanjesus.padron@intelygenz.com>
2022-12-07 11:21:19 +0000

    fix kli krt-utils

--- libs/krt-utils/pkg/krt/krt.go
+++ libs/krt-utils/pkg/krt/krt.go
@@ -2,27 +2,31 @@ package krt
 
 // File contains data about a version.
 type File struct {
+	KrtVersion  string     `yaml:"krtVersion,omitempty" validate:"omitempty"`
 	Version     string     `yaml:"version" validate:"required,resource-name,lt=20"`
 	Description string     `yaml:"description" validate:"required"`
 	Entrypoint  Entrypoint `yaml:"entrypoint" validate:"required"`
 	Config      Config     `yaml:"config" validate:"required"`
-	Nodes       []Node     `yaml:"nodes" validate:"required,dive,min=1"`
+	Nodes       []Node     `yaml:"nodes,omitempty" validate:"omitempty,dive,min=1"`
 	Workflows   []Workflow `yaml:"workflows" validate:"required,dive,min=1"`
 }
 
 // Node contains data about a version's node.
 type Node struct {
-	Name  string `yaml:"name" validate:"required,resource-name,lt=20"`
-	Image string `yaml:"image" validate:"required"`
-	Src   string `yaml:"src" validate:"required"`
-	GPU   bool   `yaml:"gpu"`
+	Name          string   `yaml:"name" validate:"required,resource-name,lt=20"`
+	Image         string   `yaml:"image" validate:"required"`
+	Src           string   `yaml:"src" validate:"required"`
+	GPU           bool     `yaml:"gpu"`
+	Subscriptions []string `yaml:"subscriptions,omitempty"` //v2
 }
 
 // Workflow contains data about a version's workflow.
 type Workflow struct {
 	Name       string   `yaml:"name" validate:"required,resource-name,lt=20"`
 	Entrypoint string   `yaml:"entrypoint" validate:"required"`
-	Sequential []string `yaml:"sequential" validate:"required"`
+	Sequential []string `yaml:"sequential,omitempty"`
+	Nodes      []Node   `yaml:"nodes,omitempty" validate:"dive"` //v2
+	Exitpoint  string   `yaml:"exitpoint,omitempty"`             //v2, once v1 deprecated make required
 }
 
 // Entrypoint defines a KRT entrypoint Image and Proto file.
