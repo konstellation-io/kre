961abd367812bc89c0068c136cc6b1743a6f2a5c
David Fuentes <david.fuentes@intelygenz.com>
2023-01-19 16:29:47 +0100

    fix: removed unneeded logs

--- engine/admin-api/domain/usecase/krt/parser/parser.go
+++ engine/admin-api/domain/usecase/krt/parser/parser.go
@@ -88,10 +88,6 @@ func (p *Parser) ParseKrtYaml(krtFilePath string) (*krt.Krt, error) {
 		return nil, fmt.Errorf("error on KRT Yaml parsing: %w", err)
 	}
 
-	for _, node := range k.Workflows[0].Nodes {
-		p.logger.Infof("Number of replicas: %d", node.Replicas)
-	}
-
 	// TODO krt-v1: deprecate retrocompatibility
 	if k.KrtVersion == "" {
 		k.KrtVersion = entity.KRTVersionV1.String()
