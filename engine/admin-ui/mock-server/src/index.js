077117f742b50d52003a39375aec98fee017d860
javier.aguilera <jaguilerapuerta@gmail.com>
2023-03-14 09:31:13 +0100

    fix(admin-ui): remove unintentional identation

--- engine/admin-ui/mock-server/src/index.js
+++ engine/admin-ui/mock-server/src/index.js
@@ -1,4 +1,4 @@
- const express = require('express');
+const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { ApolloServer } = require('apollo-server-express');
