d463185be4287ec71f64c1d3792c0a02c309fbe4
David Fuentes <david.fuentes@intelygenz.com>
2023-05-05 17:03:09 +0200

    fix: remove unused acceptance tests

--- tests/acceptance/test_krt/scripts/gen_protobuf.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-protoc -I=runtime-test-v1 \
-  --go_out=runtime-test-v1/src/go-runner/pb \
-  --python_out=runtime-test-v1/src/py-runner \
-  runtime-test-v1/public_input.proto
-
-echo "Done"
